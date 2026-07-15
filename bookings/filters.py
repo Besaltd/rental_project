eba2a7ba1d2b998f81d7f0836e3acad54e72a0c2
Ruslan <home@Mac-mini-Ruslan.fritz.box>
2026-07-15 10:27:25 +0200

    UPDATED: add query_param 'is_completed' in bookings/filters

--- bookings/filters.py
+++ bookings/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.utils import timezone
 
 from .models import Booking
 
@@ -9,7 +10,15 @@ class BookingFilter(django_filters.FilterSet):
         field_name='start_date', lookup_expr='gte')
     start_date_to = django_filters.DateFilter(
         field_name='start_date', lookup_expr='lte')
+    is_completed = django_filters.BooleanFilter(method='filter_is_completed')
 
     class Meta:
         model = Booking
-        fields = ['status', 'start_date_from', 'start_date_to']
+        fields = ['status', 'start_date_from', 'start_date_to', 'is_completed']
+
+    def filter_is_completed(self, queryset, name, value):
+        today = timezone.localdate()
+        completed = {'status': Booking.Status.CONFIRMED, 'end_date__lt': today}
+        if value:
+            return queryset.filter(**completed)
+        return queryset.exclude(**completed)
